aecfeae5e108222f7f5534a3c97acf3853aa2a75
Deeshan M <runner3222@gmail.com>
2023-04-23 17:55:42 -0400

    Compressed data from API

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -2,13 +2,17 @@
 // eslint-disable-next-line no-shadow, import/no-extraneous-dependencies
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-exports.handler = async function () {
-  const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto';
+// eslint-disable-next-line no-unused-vars
+exports.handler = async function (event, context) {
+  const eventBody = JSON.parse(event.body);
+  const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
   const response = await fetch(POKE_API);
   const data = await response.json();
 
   return {
     statusCode: 200,
-    body: JSON.stringify(data),
+    body: JSON.stringify({
+      pokemon: data.pokemon_entries,
+    }),
   };
 };
