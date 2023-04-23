08098a7b79c68144b252d445d3559aa3648362a2
Deeshan M <runner3222@gmail.com>
2023-04-23 18:15:46 -0400

    Added logs

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,4 +1,11 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { DateTime } = require('luxon');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const chalk = require('chalk');
+
 exports.handler = async function () {
+  const date = DateTime.now();
+  console.log(chalk.blue(`${date}: Hello from Netlify Functions!`));
   return {
     statusCode: 200,
     body: JSON.stringify({
--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,24 @@
 // mod.cjs
 // eslint-disable-next-line no-shadow, import/no-extraneous-dependencies
 const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { DateTime } = require('luxon');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const chalk = require('chalk');
 
 // eslint-disable-next-line no-unused-vars
 exports.handler = async function (event, context) {
   const eventBody = JSON.parse(event.body);
   const POKE_API = `https://pokeapi.co/api/v2/pokedex/${eventBody.region}`;
+
+  console.log(chalk.blue(`Fetching... ${eventBody.region}`));
+
   const response = await fetch(POKE_API);
   const data = await response.json();
+  console.log(chalk.blue('Success...'));
+  const date = DateTime.now();
+
+  console.log(chalk.blue(`User asked for: "${eventBody.region}" on [${date}]`));
 
   return {
     statusCode: 200,
